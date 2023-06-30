ee06d1cd494b4bae1c3202436884a7e664ac2550
ashfilbrun <ashesclaire@gmail.com>
2023-06-30 14:02:23 -0700

    add seed data for tasks

--- seed/tasks.js
+++ seed/tasks.js
@@ -8,22 +8,34 @@ db.on(`error`, console.error.bind(console, `MongoDB connection error:`))
 const main = async () => {
     const task = [
         {
-            taskName: 'Create layout for AUG23 mag launch'
-            userId: ,
-            description: ,
-            completed: ,
-            reviewed: ,
-            dateCreated: ,
-            dateDue: 
+            projectId: null,
+            taskName: 'Create full layout for SEPT23 issue of Dream magazine',
+            description: `Collaborate with team to dream and design the September 2023 issue of Dream magazine. Loving y2k themes with bright contrasting colors.`,
+            userId: null,
+            completed: false,
+            reviewed: false,
+            dateCreated: new Date(`05-02-2023`),
+            dateDue: new Date(`07-02-2023`)
         },
         {
-            taskName: ''
-            userId: ,
-            description: ,
-            completed: ,
-            reviewed: ,
-            dateCreated: ,
-            dateDue: 
+            projectId: null,
+            taskName: 'Pick up final edits from Yuma Gucci shoot from photographer',
+            userId: null,
+            description: `Photographers studio address: 139 W Broadway, Ste 29, NYC, NY 10012`,
+            completed: false,
+            reviewed: false,
+            dateCreated: new Date(`05-02-2023`),
+            dateDue: new Date(`07-03-2023`)
+        },
+        {
+            projectId: null,
+            taskName: 'Pick up Fendi FA23 samples from Veronica at Fendi LES',
+            userId: null,
+            description: `Veronica has big red glasses. Fendi LES address: 150 Spring St, Level 3, NYC, NY 10012`,
+            completed: false,
+            reviewed: false,
+            dateCreated: new Date(`05-02-2023`),
+            dateDue: new Date(`06-29-2023`)
         },
     ]
 }
