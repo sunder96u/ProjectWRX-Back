47106ae1a9047d3723af5362d46df45ad7690eb7
Ron <ron_rose@me.com>
2023-06-30 12:06:33 -0400

    team controller added

---
+++ controllers/teamController.js
@@ -0,0 +1,72 @@
+const { teamSchema, userSchema } = require('./models')
+
+const getAllTeams = async (req, res) => {
+    try {
+        const teams = await teamSchema.find()
+        return res.status(200).json ({ teams })
+    } catch (error) {
+        return res.status(500).json ({error:error.message})
+    }
+}
+
+
+const getTeamByName = async (req, res) => {
+    try {
+        let {id} = req.params
+        const userTeam = await userSchema.find({team: id})
+        if (!userTeam) throw Error('Team not found.')
+        res.status(200).json(userTeam)
+    } catch (e) {
+        res.status(400).send(e.message)
+    }
+}
+
+
+const createTeam = async (req, res) => {
+    try {
+        const team = await new teamSchema(req.body)
+        await team.save()
+        return res.status(201).json({
+            team,
+        })
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+    }
+}
+
+
+const updateTeam = async (req, res) => {
+    try {
+        let { id } = req.params
+        let team = await teamSchema.findByIdAndUpdate(id, req.body, {new: true})
+        if (team) {
+            return res.status(200).json(team)
+        }
+        throw new Error('Team not found.')
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+
+const deleteTeam = async (req, res) => {
+    try {
+        const { id } = req.params
+        const deleted = await teamSchema.findByIdAndDelete(id)
+        if (deleted) {
+            return res.status(200).send("Team deleted")
+        }
+        throw new Error("Team not found.")
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+
+module.exports = {
+    getAllTeams,
+    getTeamByName,
+    createTeam,
+    updateTeam,
+    deleteTeam
+}
