12c3c4a760eeee0ec41df24c476e9832169dd5bb
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-06-30 08:11:14 -0500

    added files and connected to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const PORT = process.env.PORT || 3001
+const db = require('./db')
 
 const app = express()
 
@@ -8,6 +9,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Routes go here
 
+app.get('/', () => {
+      console.log('hello world')
+})
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
