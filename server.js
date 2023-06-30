43edc077da827e82ed58884b6540000b673646ba
Steven Underwood <stevenunderwood2@hotmail.com>
2023-06-30 11:03:05 -0500

    create google OAuth

--- server.js
+++ server.js
@@ -2,15 +2,36 @@ const express = require('express')
 const cors = require('cors')
 const PORT = process.env.PORT || 3001
 const db = require('./db')
+const logger = require('morgan')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const passport = require('passport')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(function (req, res, next) {
+      res.locals.user = req.user
+      next()
+})
 
 
-app.get('/', () => {
-      console.log('hello world')
+app.get('/', (req, res) => {
+      res.send(`Hello world`)
 })
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
